7eb52d754c13cf8fa226f7f000a199ffdaffd7d1
AI REPOS 23 <93036340+ai-botz@users.noreply.github.com>
2024-06-07 12:20:49 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,16 @@
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Backup Channel @JishuBotz
-# Developer @JishuDeveloper
 
 
 
 import os
 
 class Config:
-    API_ID = os.environ.get("API_ID", "")
-    API_HASH = os.environ.get("API_HASH", "")
+    API_ID = os.environ.get("API_ID", "25443947")
+    API_HASH = os.environ.get("API_HASH", "ab4cd800dac7c9a36314ee83800adba8")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
-    BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
-    DB_URL = os.environ.get("DB_URL", "")
-    DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
-    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '').split()]
+    BOT_SESSION = os.environ.get("BOT_SESSION", "ai-forward-bot") 
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://haroonhassan3:haroonhassan3@haroonhassan3.7zzzhoe.mongodb.net/?retryWrites=true&w=majority")
+    DB_NAME = os.environ.get("DB_NAME", "haroonhassan3")
+    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '6013614984').split()]
 
 
 class temp(object): 
@@ -24,15 +19,3 @@ class temp(object):
     forwardings = 0
     BANNED_USERS = []
     IS_FRWD_CHAT = []
-    
-
-
-
-
-
-
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Backup Channel @JishuBotz
-# Developer @JishuDeveloper
